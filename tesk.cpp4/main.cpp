245e49e143e3d19812fce9e4e7930576c940b8e6
zengkaiwei2018 <1912207369@qq,com>
2019-06-26 20:21:48 +0800

    Initial Commit

---
+++ tesk.cpp4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tesk.cpp4
+//
+//  Created by s20181105306 on 2019/6/26.
+//  Copyright © 2019 s20181105306. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
