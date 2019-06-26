97f7dbed2a931d1c3053a4929b791dcca61e79a7
zengkaiwei2018 <1912207369@qq,com>
2019-06-26 20:38:29 +0800

    准备输出学生比赛数据

--- tesk.cpp4/main.cpp
+++ tesk.cpp4/main.cpp
@@ -1,15 +1,129 @@
+
 //
 //  main.cpp
-//  tesk.cpp4
+//  tesk.cpp
 //
-//  Created by s20181105306 on 2019/6/26.
+//  Created by s20181105306 on 2019/6/20.
 //  Copyright © 2019 s20181105306. All rights reserved.
 //
 
-#include <iostream>
+//#include <iostream>
 
+//int main(int argc, const char * argv[]) {
 int main(int argc, const char * argv[]) {
+    //int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+    //std::cout << "Hello, World!\n";
+    // return 0;
+    //std::cout << "Hello, World!\n";
+    // return 0;
+#include <iostream>
+    
+    //int main(int argc, const char * argv[]) {
+    // insert code here...
+    //std::cout << "Hello, World!\n";
+    //eturn 0;
+    //}
+#include <fstream>
+    
+    //int main(int argc, const char * argv[]) {
+    // insert code here...
+    //std::cout << "Hello, World!\n";
+    //eturn 0;
+    //}
+#include<iostream>
+#include<cstring>
+#include<fstream>
+#include<stdlib.h>
+#include<iomanip>
+#include"string.h"
+#include"stdlib.h"
+    using namespace std;
+    struct student
+    int main(){
+        ofstream count(/Users/s20181105306/Desktop/test.cpp);
+        ifstream int(/Users/s20181105306/Desktop/选手信息的副本.txt)
+        using namespace std;
+        struct A
+        {
+            string school;
+            string number;
+            string name;
+            string core;
+            float avescore;
+        }
+        student[20];
+        struct B;
+        {
+            string name;
+            float f_point[20];
+        }judge[7];
+        int main();
+        {
+            static int add=0;
+            string point;
+            ifstream fp1("C:\\Users\\¿ªÎ°\Desktop\\ÎÄ¼þ2.txt");
+        }
+        static int add=0;
+        do{
+            getline(fp1,student[add].school,' ');
+            getline(fp1,student[add].number,' ');
+            getline(fp1,student[add].name,'\n');add++;
+        }while(!fp1.eof());
+        fp1.close();
+        ifstream fp1("C:\\Users\\•û¿ªÎ°\\Desktop\\ÎÄ¼þ1.txt");
+        do{
+            getline(fp2,jude[add2].name,' ');
+            int j=0;
+            peo_num=add;
+            {
+                for(;peo_num=add>1;peo_num--)
+                {
+                    getline(fp2,point,' ');
+                    jude[add2].f_point[j]=atoi(point.a_str());j++;
+                }
+                getline(fp2,point,'\n');
+                jude[add2].f_point[j]=atoi(point.c_str());add2++;
+            }
+        }while(add2!=7);
+        fp2.close(); float G_point[7];
+        for(int k=0;k<add;k++)
+        {
+            if(int.is_open()){
+                char num[10];
+                char sex[5];
+                char age[2];
+                int remark;
+                struct student*prior;
+                struct student*next;}l;
+            struct student*start*,*last;
+            void enter(),list(),save(),
+            load(),delet_();
+            void(),delete_();
+            void store(struct student*g);
+            meun,score();
+            
+            
+            
+            
+            
+            
+            couut.close();
+            for(int i=0;i<7;i++)
+            {
+                G_point[i]=jude[i].f_point[k];
+            }
+            int substitute;
+            for(j=0;j<5;j++)
+            {
+                for(int i=0;i<7;i++)
+                {
+                    if(G_point[i+1]<G_point[i])
+                        G_point[i+1]=G_point[i];
+                    G_point[i]=substitute;
+                }
+            }
+        }
+        stu[i].avepoint=(G_point[1]+G_point[2]+G_point+G_point[3]+G_point[4]+G_point[5])
+    }
+    ofstream fp3("")
